82243ed7e066cf1c690c057d7e456d77835c279a
VerstandTsai <hackertsaigenz@gmail.com>
2023-07-06 02:05:14 +0800

    Made the ball in demo have a better resolution

--- test.cpp
+++ test.cpp
@@ -20,7 +20,7 @@ int main() {
     scene["cuboid"].position = Vec3(-2, 0, 0);
     scene["cuboid"].color = Vec3(1, 1, 0);
 
-    scene["sphere"] = Sphere();
+    scene["sphere"] = Sphere(1, 100);
     scene["sphere"].position = Vec3(0, 0, 0);
     scene["sphere"].color = Vec3(1, 0.2, 0.2);
 
