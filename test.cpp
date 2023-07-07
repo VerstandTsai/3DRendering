db01520afa4268d5b8e5658deeb7bd20826edee2
VerstandTsai <hackertsaigenz@gmail.com>
2023-07-07 10:39:17 +0800

    Better Demo

--- test.cpp
+++ test.cpp
@@ -4,14 +4,36 @@
 using namespace proxima;
 using namespace std::chrono;
 
+void control(Window &window, Camera &camera) {
+    Vec3 right = cross(camera.normal(), Vec3(0, 1, 0)).normalized();
+    Vec3 front = cross(Vec3(0, 1, 0), right);
+
+    if (window.keydown(W))
+        camera.position += front;
+    if (window.keydown(S))
+        camera.position -= front;
+    if (window.keydown(A))
+        camera.position -= right;
+    if (window.keydown(D))
+        camera.position += right;
+    if (window.keydown(SPACE))
+        camera.position += Vec3(0, 1, 0);
+    if (window.keydown(LSHIFT))
+        camera.position -= Vec3(0, 1, 0);
+
+    camera.euler_angles -= Vec3(window.mouse_dy(), window.mouse_dx(), 0);
+}
+
 int main() {
     int width = 1280;
     int height = 720;
     Window window(width, height);
     Renderer renderer(width, height);
-    Scene scene(Vec3(0.5, 0.8, 1));
+
+    Scene scene(Vec3(0, 0, 0));
 
     scene.camera.position = Vec3(0, 0, 8);
+
     scene["sun"] = new PointLight(10000, Vec3(1, 1, 1), Vec3(0, 100, 0));
     scene["light1"] = new PointLight(10, Vec3(1, 0, 0), Vec3(0, 5, 0));
     scene["light2"] = new PointLight(10, Vec3(0, 1, 0), Vec3(6, 0, 0));
@@ -34,24 +56,6 @@ int main() {
     scene["teapot"]->color = Vec3(1, 0.5, 0.5);
 
     while (!window.closed()) {
-        Vec3 right = cross(scene.camera.normal(), Vec3(0, 1, 0)).normalized();
-        Vec3 front = cross(Vec3(0, 1, 0), right);
-
-        if (window.keydown(W))
-            scene.camera.position += front;
-        if (window.keydown(S))
-            scene.camera.position -= front;
-        if (window.keydown(A))
-            scene.camera.position -= right;
-        if (window.keydown(D))
-            scene.camera.position += right;
-        if (window.keydown(SPACE))
-            scene.camera.position += Vec3(0, 1, 0);
-        if (window.keydown(LSHIFT))
-            scene.camera.position -= Vec3(0, 1, 0);
-
-        scene.camera.euler_angles -= Vec3(window.mouse_dy(), window.mouse_dx(), 0);
-
         scene["light1"]->position = rotate(scene["light1"]->position, Vec3(1, 0, 0));
         scene["light2"]->position = rotate(scene["light2"]->position, Vec3(0, 1, 0));
         scene["light3"]->position = rotate(scene["light3"]->position, Vec3(0, 0, 1));
@@ -61,6 +65,7 @@ int main() {
         scene["sphere"]->euler_angles += Vec3(0, 5, 0);
         scene["teapot"]->euler_angles += Vec3(3, 5, 4);
 
+        control(window, scene.camera);
         auto start = high_resolution_clock::now();
         window.draw(renderer.render(scene));
         auto end = high_resolution_clock::now();
